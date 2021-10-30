2723c0df60b5e9aa78546ab939693ef785970ce8
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-10-30 12:31:49 +0530

    Example of pointcut declaration

--- src/com/spring/aop/basics/aspects/LoggingAspect.java
+++ src/com/spring/aop/basics/aspects/LoggingAspect.java
@@ -2,14 +2,31 @@ package com.spring.aop.basics.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-	@Before("execution(public void addAccount())")
+	// Example of @Before advice
+	// execution(modifier-pattern? Return-type declaring-type-pattern?
+	// Method-name-pattern(param-pattern) throws-pattern?)
+	// Pattern is optional if it has ?
+	// For param-pattern we can give:
+	// (): method with no args
+	// (*): 1 arg of any type
+	// (..): 0 or any number of args of any type
+	// To match specific class method we need to 852give fully qualified name
+	@Before("forDAOPackages()")
 	public void beforeAdviceAccountDAO() {
-		System.out.println("From before advice");
+		System.out.println("From before advice pointcut declaration");
 	}
+
+	// Example of PointCut declaration
+	@Pointcut("execution(public void addAccount())")
+	private void forDAOPackages() {
+
+	}
+
 }
