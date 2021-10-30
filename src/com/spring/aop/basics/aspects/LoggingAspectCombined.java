ba65c6fdd42a4b3f286e10a32dd4df02fc7810b1
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-10-30 22:02:31 +0530

    Example of AfterRetruning advice

--- src/com/spring/aop/basics/aspects/LoggingAspectCombined.java
+++ src/com/spring/aop/basics/aspects/LoggingAspectCombined.java
@@ -3,10 +3,13 @@ package com.spring.aop.basics.aspects;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.spring.aop.basics.beans.Account;
 
 @Aspect
 @Component
@@ -15,11 +18,22 @@ public class LoggingAspectCombined {
 
 	// Example of combining PointCut declarations
 	@Before("com.spring.aop.basics.aspects.AOPExpressions.forDAOPackages() || com.spring.aop.basics.aspects.AOPExpressions.forGetters()")
-	public void beforeAdviceGetter(JoinPoint joinPoint) {
+	public void beforeAdviceGetterAndDAO(JoinPoint joinPoint) {
 		String methodSignature = joinPoint.getSignature().toShortString();
 		System.out
 		.println("From before advice combining pointcut declarations for method signature: " + methodSignature);
 		Object[] methodArgs = joinPoint.getArgs();
 		System.out.println("Following are the method args: " + Arrays.toString(methodArgs));
+	}
+
+	// Example of AfterRetruning advice
+	// Return type has to match then only advice will execute
+	// Advice will run after method execution is completed(successful execution)
+	// We can also have pointcut="execution(* com.real.vivek.dao.*.get*(..))" in
+	// @AfterReturning instead of value
+	@AfterReturning(value = "com.spring.aop.basics.aspects.AOPExpressions.forDAOPackages() || com.spring.aop.basics.aspects.AOPExpressions.forGetters()", returning = "account")
+	public void afterAdviceGetterAndDAO(JoinPoint joinPoint, Account account) {
+		System.out.println("From after retruning advice");
+		System.out.println("The return value is " + account);
 	}
 }
